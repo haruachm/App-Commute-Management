b6378251f15fa8a97f4f5d3b748cb50d8abd2748
haruachm <horang_e7@naver.com>
2023-03-21 23:36:43 +0900

    fix : iOS Improved security

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyABIsRmVOwDhekOmGN19K4nxdvfzyO8V8k")
+    GMSServices.provideAPIKey(Storage().googleMapApiKey)
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
